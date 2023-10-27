3cda49413965be24a2df300befaaac7892f8b7f5
Max te Brake <m.a.j.t.brake@student.tue.nl>
2023-10-27 18:52:21 +0200

    modified:   Main.java 	modified:   MainMenu.java

--- Main.java
+++ Main.java
@@ -3,15 +3,20 @@ import javax.swing.*;
 public class Main {
     private static int WINDOW_WIDTH = 400;
     private static int WINDOW_HEIGHT = 600;
-    
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Brick Breaker Pong 2IP90");
-        GamePlay gamePlay = new GamePlay(WINDOW_WIDTH, WINDOW_HEIGHT);
-        frame.add(gamePlay);
+
+        MainMenu menu = new MainMenu();
+        frame.add(menu);
+
+        // GamePlay gamePlay = new GamePlay(WINDOW_WIDTH, WINDOW_HEIGHT);
+        // frame.add(gamePlay);
+        // frame.addKeyListener(gamePlay);
+
         frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setVisible(true);
-        frame.addKeyListener(gamePlay);
     }
 }
--- MainMenu.java
+++ MainMenu.java
@@ -1,5 +1,4 @@
 import java.awt.*;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -8,6 +7,36 @@ import javax.swing.*;
 /**
  * 
  */
-public class MainMenu extends JPanel implements ActionListener {
-    
+public class MainMenu extends JPanel {
+    private JButton playButton;
+
+    public MainMenu() {
+        initializeComponents();
+    }
+
+    private void initializeComponents() {
+        setLayout(new BorderLayout());
+
+        // Create a panel to center the play button
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        // Create the "Play" button
+        playButton = new JButton("Play");
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO
+                // Add action to start the game when the "Play" button is clicked
+                // You can switch to the game screen here
+            }
+        });
+
+
+        // Add the play button to the button panel
+        buttonPanel.add(playButton);
+
+        // Add the button panel to the center of the main menu panel
+        add(buttonPanel, BorderLayout.CENTER);
+    }
 }
